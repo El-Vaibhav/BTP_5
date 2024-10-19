f3da5c9a52ecda45b867e3eb180191ebbbb8dcd7
El-Vaibhav <135622906+El-Vaibhav@users.noreply.github.com>
2024-10-19 11:12:45 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import pywt
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
