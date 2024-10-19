4420b74d3fe8cc4211e5def10188818335a0f25a
El-Vaibhav <135622906+El-Vaibhav@users.noreply.github.com>
2024-10-19 12:33:15 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pywt
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
